ccf4b334fdc8d570e37340a92dead6ca658a4a2f
Tilmann Singer <tils@tils.net>
2017-09-16 21:43:40 +0200

    Animate by adding blurry circles, controlled by random data

--- index.js
+++ index.js
@@ -13,3 +13,76 @@ const map = new gl.Map({
 map.addControl(new gl.NavigationControl(), 'top-left')
 
 const el = document.getElementById('map')
+
+var x = 13.5, y = 52.5, pm = 10, toxicity = 0
+
+var path = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [x, y]
+			},
+			"properties": {
+				"pm": pm,
+				"toxicity": 0
+			},
+		},
+	]
+}
+
+map.on('load', function() {
+
+	map.addSource('path', {
+		"type": "geojson",
+		"data": path
+	})
+
+	map.addLayer({
+		"id": "path",
+		"source": "path",
+		"type": "circle",
+		"paint": {
+			"circle-color": {
+				"property": "toxicity",
+				"type": "categorical",
+				"stops": [
+					[0, "blue"],
+					[1, "yellow"],
+					[2, "red"],
+				]
+			},
+			"circle-blur": 1,
+			"circle-radius": {
+				"property": "pm",
+				"type": "identity"
+			}
+		}
+	})
+
+	const animate = function() {
+		x = x + (Math.random() - 0.2) * 0.00005
+		y = y + (Math.random() - 0.2) * 0.00005
+		pm = pm + (Math.random() - 0.5) * 3
+		toxicity = Math.round(Math.random() * 3)
+
+		path.features.push({
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [x, y]
+			},
+			"properties": {
+				"pm": pm,
+				"toxicity": toxicity
+			}
+		})
+
+		map.getSource('path').setData(path)
+		requestAnimationFrame(animate)
+	}
+
+	animate()
+})
